41e20d17c364200d9edc4d4b744b684d78c05e29
Joshua Cook <me@joshuacook.me>
2017-12-04 19:07:32 -0800

    cleanup

--- __init__.py
+++ __init__.py
@@ -1,11 +1,4 @@
-import quandl
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-
-quandl.ApiConfig.api_key = '9Mj_SA7bCxBrUSssNpHL'
-dow_jones = ['MMM', 'AXP' 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'KO', 'DIS', 'DD', 'XOM', 'GE', 
-             'GS', 'HD', 'IBM', 'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT', 'NKE', 'PFE', 'PG', 
-             'TRV', 'UTX', 'UNH', 'VZ', 'V', 'WMT']
-             
